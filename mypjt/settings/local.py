f649660d65b3509357629d2707a4652c4fd91897
SeunghoShin93 <tmdghshin@gmail.com>
2021-04-04 22:38:00 +0900

    EDIT intro design | ADD logo

--- mypjt/settings/local.py
+++ mypjt/settings/local.py
@@ -1,3 +1,13 @@
 from .base import *
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '3jo',
+        'USER': '3jo',
+        'PASSWORD': '3jo',
+        'HOST': '192.168.99.100',
+        'PORT': '33066',
+    }
+}
